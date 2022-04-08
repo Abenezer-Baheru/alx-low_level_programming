04b46185d7e3c17557def4ef39842dacb3393706
Abenezer-Baheru <abeniug2001@gmail.com>
2022-04-08 02:40:51 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * Return: 0 Always Success(Correct)
+ */
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar('\n');
+}
+
+return (0);
+}
